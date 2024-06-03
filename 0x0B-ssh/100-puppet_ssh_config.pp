917a37358a73427c096f7c45b5d5b2ac23cd90b2
khireddinemahaline <khirozr@gmail.com>
2024-06-03 14:47:07 +0100

    ssh configure file

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,16 +1,16 @@
 include stdlib
 # change some configration file arg
 
-file_line {'Turn off passwd auth':
+file_line { 'Turn off passwd auth':
     ensure  => present,
-    path    => 'etc/ssh/ssh_config',
+    path    => '/etc/ssh/ssh_config',
     line    => '   PasswordAuthentication no',
     replace => true,
 }
 
-file_line {'delare identify file':
+file_line { 'delare identify file':
     ensure  => present,
-    path    => 'etc/ssh/ssh_config',
+    path    => '/etc/ssh/ssh_config',
     line    => '     IdentityFile ~/.ssh/school',
     replace => true,
 }
