ab384c989f0884726d382103c668e1c9a33380ff
khireddinemahaline <khirozr@gmail.com>
2024-07-03 08:20:20 -0700

    puppt ngnix

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,6 @@
+#install ngnix using puppt
+
+excu {'ngnix'
+provider  => 'shell',
+command   => 'apt-get -y update; apt-get -y install nginx; echo "Hello World" | sudo tee /var/www/html/index; sudo sed -i \'/listen 80 default_server/a rewrite ^/redirect_me https://www.youtube.com/ permanent;\' /etc/nginx/sites-available/default sudo service nginx restart',
+}
